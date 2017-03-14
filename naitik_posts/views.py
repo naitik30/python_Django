aaa3046ff6309dafb0e893dbfee108bc59bec074
naitik30 <patelnaitik30@gmail.com>
2017-03-14 16:24:58 -0700

    second commit

--- naitik_posts/views.py
+++ naitik_posts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def post_create(request):
-    return HttpResponse("<h1>Create<h1>")
+    return HttpResponse("<h1>Create First Page<h1>")
 def post_details(request):
-    return HttpResponse("<h1>Details<h1>")
+    return HttpResponse("<h1>Details of All post<h1>")
 def post_list(request):
-    return HttpResponse("<h1>List<h1>")
+    return HttpResponse("<h1>List view of all post<h1>")
 def post_update(request):
-    return HttpResponse("<h1>Update<h1>")
+    return HttpResponse("<h1>Update the selected post<h1>")
 def post_delete(request):
-    return HttpResponse("<h1>Delete<h1>")
+    return HttpResponse("<h1>Delete the selected post<h1>")
